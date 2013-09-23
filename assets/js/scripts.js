3b78b3795215e58f7df839f41bbadc4c930bf584
luyzgarcia <luyzgarcia@Mac-Luiz.local>
2013-09-23 17:24:46 -0300

    Alteraçoes no site pra rodar no IE 9

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,180 +1,98 @@
-$(document).ready(function() {
-	ajustatamanhos();
-	//Validar formulario
-	jQuery(".form_contato").validationEngine({
-		ajaxFormValidation: true
-	}); 
-});
-$(window).resize(function() {
-	ajustatamanhos();
-});
-
-function moveandar(andar) {
-	if(andar == '#cobertura') {
-		$('.andar p').stop().show('slow').animate({marginTop: "10px"});
-	}else if(andar == '#apartamentos') {
-		$('.andar p').stop().show('slow').animate({marginTop: "40px"});
-	}else if(andar == '#comerciais') {
-		$('.andar p').stop().show('slow').animate({marginTop: "80px"});
-	}
-}
-
-function mostrarMenu() {
-	$('.nav').show('slow');	
-}
-
-function moverpagina(anchor) {
-	executou = false;
-	var $anchor = $(anchor);
-	$($anchor.attr('href')).css('opacity', '0');
-	
-	$('html, body').stop().animate({
-	    scrollLeft: $(anchor.attr('href')).offset().left
-	}, 1000 , function () {
-		if(!executou) {
-			executou = true;
-   			$($anchor.attr('href')).find('.inner .detalhe').fadeIn().css('display','inline-table');
-   			moveandar($anchor.attr('href'));
-   			
-   			$($anchor.attr('href')).stop().animate({opacity: 1},1500);
-   			$($anchor.attr('href')).find('.back').stop().animate({opacity: 0.7},800);
-		
-			
-			if($anchor.attr('href') == '#home') {
-   				$('.bt_left a').prop('href', '#home');
-   				$('.bt_right a').prop('href', '#cobertura');
-   				return;
-			}else if($anchor.attr('href') == '#cobertura') {
-				$('.bt_left a').prop('href', '#home');
-				$('.bt_right a').prop('href', '#apartamentos');
-				return;
-			}else if($anchor.attr('href') == '#apartamentos') {
-				$('.bt_left a').prop('href', '#cobertura');
-				$('.bt_right a').prop('href', '#comerciais');
-				return;
-			}else if($anchor.attr('href') == '#comerciais') {
-				$('.bt_left a').prop('href', '#apartamentos');
-				$('.bt_right a').prop('href', '#localizacao');
-				return;
-			}else if($anchor.attr('href') == '#localizacao') {
-				$('.bt_left a').prop('href', '#comerciais');
-				return;
-			}
-		}
-	});
-	event.preventDefault();
-}
-
-
 $(function() {
-	$('html, body').stop().animate({
-	    scrollLeft: $('#home').offset().left
-	}, 1000);
-	
-	$('.logo_elvira').click(function () {
-		$(this).next(".section").toggle();
-	});
-	
-	$('.bt_right a, .bt_left a').bind('click', function (event) {
-		var $anchor = $(this);
-		moverpagina($anchor);
-	});
- $('ul.nav a').bind('click',function(event){
-     var $anchor = $(this);
-     moverpagina($anchor);
- });
- 
- var mapOptions = {
- 	zoom: 5,
- 	center: new google.maps.LatLng(-33, 151),
- 	mapTypeId: google.maps.MapTypeId.ROADMAP
- }
- var map = new google.maps.Map($('#mapa', mapOptions));
- 
-});
-
-/*!3d  -24.725515,-53.741969*/
-function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(-24.725515,-53.731969),
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("mapa"),
-            mapOptions);
-        
-        var Latlng = new google.maps.LatLng(-24.725515,-53.741969); 	        
-        var image = 'assets/images/icon_mapa.png';
-        var marker = new google.maps.Marker({
-            position: Latlng,
-            map: map,
-            title: 'Residencial Dona Elvira',
-            icon: image
-          });
-         
-        /*var proizLatlng = new google.maps.LatLng(-24.7319617,-53.7379156);  
-        var markerProiz = new google.maps.Marker({
-        	position: proizLatlng,
-        	map: map,
-        	title: 'Proiz',
-        	icon: 'assets/images/logo_proiz.png'
-        });*/
-      }
-google.maps.event.addDomListener(window, 'load', initialize);
-
-
-jQuery(document).ready(function(){
-
-	$('.form_contato').submit(function() {
-		
-		$(this).find('p').text('Enviando...');
-		
-		var dados = jQuery( this ).serialize();
-		
-		jQuery.ajax({
-			type: 'POST',
-			url: "enviar.php",
-			data: dados,
-			success: function( data ) {
-				alert(data);
-			}
-		});
-		$(this).find(':input').each(function () {
-			switch(this.type) {
-			case 'text':
-			case 'textarea':
-				$(this).val('');
-				break;
-			}
-			
+		jQuery(".form_contato").validationEngine({
+			ajaxFormValidation: true
 		});
 		
-		$(this).find('p').text('Email enviado');
+		$('ul.nav a').bind('click',function(event){
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollLeft: $($anchor.attr('href')).offset().left
+			}, 1000);
+			event.preventDefault();
+		});
 		
-		return false;
+		$('html, body').stop().animate({
+			scrollLeft: $('#home').offset().left
+		}, 1000);
+		
+		$('.bt_right a, .bt_left a').bind('click', function (event) {
+			var $anchor = $(this);
+			moverpagina($anchor);
+		});
+		 $('ul.nav a').bind('click',function(event){
+			 var $anchor = $(this);
+			 moverpagina($anchor);
+		 });
+		
+		$(".bt_contato a").click(function(e) {
+			var formulario = $(this).data('formulario');
+			$("#"+formulario).toggle('slow');
+		});
+		
+		//Coloca o map na posicao
+		var mapOptions = {
+			zoom: 5,
+			center: new google.maps.LatLng(-33, 151),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		 }
+		 var map = new google.maps.Map($('#mapa', mapOptions));
+		
+		$('.form_contato').submit(function() {
+			$(this).find('p').text('Enviando...');
+			var dados = jQuery( this ).serialize();
+			jQuery.ajax({
+				type: 'POST',
+				url: "enviar.php",
+				data: dados,
+				success: function( data ) {
+					alert(data);
+				}
+			});
+			$(this).find(':input').each(function () {
+				switch(this.type) {
+				case 'text':
+				case 'textarea':
+					$(this).val('');
+					break;
+				}
+			});
+			$(this).find('p').text('Email enviado');
+			return false;
+		});
+		
 	});
-});
-
-function ajustatamanhos() {
-	larguratela = $(window).width();
-	alturatela = $(window).height();
-	$('.inner').css('width', (larguratela - 60 ));
-	$('.inner').css('height',(alturatela - 60 ));
-}
-
-$(document).ready(function() {
-	$(".icones img[title]").tooltips();
-	
-	$(".nav_mini").click(function() {
-		mostrarMenu();
+	$(document).ready(function() {
+		$(".icones img[title]").tooltips();
+		
+		$(".zoom").elevateZoom({
+			zoomWindowWidth:550,
+			zoomWindowHeight:450
+		});
+		ajustatamanhos();
+	});
+	$(window).resize(function() {
+		ajustatamanhos();
 	});
 	
-	$(".bt_contato a").click(function(e) {
-		var formulario = $(this).data('formulario');
-		$("#"+formulario).toggle('slow');
-	});
+	function ajustatamanhos() {
+		larguratela = $(window).width();
+		alturatela = $(window).height();
+		$('.inner').css('width', (larguratela - 60 ));
+		$('.inner').css('height',(alturatela - 60 ));
+	}
 	
-	$('.imagens_cobertura .grupo').click(function () {
+	
+	
+	$(document).ready(function() {
+		
+		$('.planta_cobertura').click( function() {
+			ocultarCoberturas();
+			var selt = $(this);
+			togglePlanta(true, selt);
+			//mostrarMenu();
+		});	
+		
+		$('.imagens_cobertura .grupo').click(function () {
 		
 		togglePlanta(false);	
 		
@@ -208,39 +126,13 @@ $(document).ready(function() {
 			//$('.nav').hide('slow');
 		}
 	});
-	
-	$('.planta_cobertura').click( function() {
-		ocultarCoberturas();
-		var selt = $(this);
-		togglePlanta(true, selt);
-		mostrarMenu();
+		
 	});
 	
-	
-	
-	function ocultarCoberturas() {
-		$('.imagens_cobertura .grupo img').removeClass('esticada');
-		$('.imagens_cobertura .grupo img').removeClass('imagem_grande');
-		$('.imagens_cobertura .grupo').children('.fundo_verde').show('slow');
-		$('.imagens_cobertura').stop().animate({marginTop: "0px"});
-	}
-	
 	function togglePlanta(ocultar, self) {
-		//$(self).children('.planta').text('aaa');
 		if(typeof(ocultar) === 'undefined') ocultar = true;
 		if(ocultar) {
-			$($(self).children('.planta')).toggle( 'slow',function () {
-				/*if ($(this).is(":visible")) {
-					//Adiciona o css 
-					$('#bt_verplanta h4').text('Esconder planta');
-					$( "#bt_verplanta" ).addClass( "ver_planta_ativo" );
-			    } else {
-			        //Remove o css
-			        $('#bt_verplanta h4').text('Ver planta');
-			        $( "#bt_verplanta" ).removeClass( "ver_planta_ativo" );
-			    } */
-			    //alert('toggle');
-			} );
+			$($(self).children('.planta')).toggle( 'slow',function () {} );
 		}else {
 			$('#planta').hide('slow', function() {
 				$('#bt_verplanta h4').text('Ver planta');
@@ -248,4 +140,91 @@ $(document).ready(function() {
 			});
 		}
 	}
-});	
+	
+	function ocultarCoberturas() {
+		$('.imagens_cobertura .grupo img').removeClass('esticada');
+		$('.imagens_cobertura .grupo img').removeClass('imagem_grande');
+		$('.imagens_cobertura .grupo').children('.fundo_verde').show('slow');
+		$('.imagens_cobertura').stop().animate({marginTop: "0px"});
+	}
+	function moverpagina(anchor) {
+		executou = false;
+		var $anchor = $(anchor);
+		$($anchor.attr('href')).css('opacity', '0');
+		
+		$('html, body').stop().animate({
+			scrollLeft: $(anchor.attr('href')).offset().left
+		}, 1000 , function () {
+			if(!executou) {
+				executou = true;
+				$($anchor.attr('href')).find('.inner .detalhe').fadeIn().css('display','inline-table');
+				moveandar($anchor.attr('href'));
+				
+				$($anchor.attr('href')).stop().animate({opacity: 1},1500);
+				//$($anchor.attr('href')).find('.back').stop().animate({opacity: 0.7},800);
+				
+				if($anchor.attr('href') == '#home') {
+					$('.bt_left a').prop('href', '#home');
+					$('.bt_right a').prop('href', '#cobertura');
+					return;
+				}else if($anchor.attr('href') == '#cobertura') {
+					$('.bt_left a').prop('href', '#home');
+					$('.bt_right a').prop('href', '#apartamentos');
+					return;
+				}else if($anchor.attr('href') == '#apartamentos') {
+					$('.bt_left a').prop('href', '#cobertura');
+					$('.bt_right a').prop('href', '#comerciais');
+					return;
+				}else if($anchor.attr('href') == '#comerciais') {
+					$('.bt_left a').prop('href', '#apartamentos');
+					$('.bt_right a').prop('href', '#localizacao');
+					return;
+				}else if($anchor.attr('href') == '#localizacao') {
+					$('.bt_left a').prop('href', '#comerciais');
+					return;
+				}
+			}
+		});
+		event.preventDefault();
+	}
+	function moveandar(andar) {
+		if(andar == '#home') {
+			$('.andar p').stop().hide().animate({marginTop: "0px"});		
+		}else if(andar == '#cobertura') {
+			$('.andar p').stop().show('slow').animate({marginTop: "10px"});
+		}else if(andar == '#apartamentos') {
+			$('.andar p').stop().show('slow').animate({marginTop: "40px"});
+		}else if(andar == '#comerciais') {
+			$('.andar p').stop().show('slow').animate({marginTop: "80px"});
+		}else if(andar == '#localizacao') {
+			$('.andar p').stop().hide();
+		}
+	}
+	//Geração do mapa com a logo da empresa
+	function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(-24.725515,-53.731969),
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("mapa"),
+            mapOptions);
+        
+        var Latlng = new google.maps.LatLng(-24.725515,-53.741969); 	        
+        var image = 'assets/images/icon_mapa.png';
+        var marker = new google.maps.Marker({
+            position: Latlng,
+            map: map,
+            title: 'Residencial Dona Elvira',
+            icon: image
+          });
+         
+        /*var proizLatlng = new google.maps.LatLng(-24.7319617,-53.7379156);  
+        var markerProiz = new google.maps.Marker({
+        	position: proizLatlng,
+        	map: map,
+        	title: 'Proiz',
+        	icon: 'assets/images/logo_proiz.png'
+        });*/
+      }
+google.maps.event.addDomListener(window, 'load', initialize);
